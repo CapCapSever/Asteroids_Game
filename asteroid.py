0a6b7095735e8dc59040a6d042af65cb95e20f72
TraiNguyenVan <traipupgmobile@gmail.com>
2026-05-10 22:36:16 +0700

    feat: implement player movement, asteroid spawning, and collision detection systems

---
+++ asteroid.py
@@ -0,0 +1,12 @@
+import pygame
+from circleshape import CircleShape
+from constants import LINE_WIDTH
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    def draw(self, screen):
+        pygame.draw.circle(surface=screen, color="white",width=LINE_WIDTH, center=self.position, radius=self.radius)
+        
+    def update(self, dt):
+        self.position += self.velocity*dt
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
 import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
 
+from logger import log_state
+from logger import log_event
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids...")
@@ -10,13 +15,33 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidField =  AsteroidField()
+    player = Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for obj in asteroids:
+            if obj.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+                
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000        
 
 
 if __name__ == "__main__":
